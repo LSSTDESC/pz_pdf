3007ec11152944f7f534201e090b4847c52a204f
William Hartley <willliam.hartley@gmail.com>
2019-01-24 14:51:00 +0000

    Tidied up script to reform the BPZ outputs into an HDF5 file of PDFs. File naming is done via data source, e.g. DC2, and chunks of objects.

--- pz/pdf/bpz_wrapper/reform_h5_bpz.py
+++ pz/pdf/bpz_wrapper/reform_h5_bpz.py
@@ -6,46 +6,42 @@ import yaml
 import subprocess
 
 """
-call like:
-python reform_h5_bpz.py <yaml-file> <bpz-prob-file>
-
-(Should write in python3)
 Take BPZ output prob file (in ascii by default) and turn it into standard style hdf5 file, to go into pdf_combine.
 
-May as well take filename as argument.
-Output name is derived from input name.
+Output name must include code identifier (BPZ), perhaps version, some info regarding the data source (cosmoDC2) and perhaps some idea of the block of objects run.
 
-h5 file should have two datasets, /ID/ and /PDF/.
+h5 file should have two datasets, /ID/ and /PDF/. Can add a further info - date, data blocks, version etc. as attributes of /ID/?
 """
 
-fname = sys.argv[2]
-out_name = fname.split('.')+'.h5'
+def reform_bpz(block_num, block_size):
 
-config = yaml.load(open(sys.argv[1]))
+    config = yaml.load(config_file)
 
-probs = np.loadtxt(fname)
-ID = copy.copy(probs[:,0])
-probs = probs[:,1:]
+    fname = 'BPZ_{0}_{1}_{2}.probs'.format(config['SOURCE'][0], block_num, block_size)
+    out_name = fname.split('.')+'.h5'
 
-# switch this to come from yaml (have a single pipeline yaml, together
-# with prep_cat)
-# z-array is assumed to be linear intervals in redshift.
-# [n_interval, centre of lowest bin, centre of highest bin]
-zarr = [config['N_BIN'], config['MIN_z'], config['Dz']]
+    probs = np.loadtxt(fname)
+    ID = copy.copy(probs[:,0])
+    probs = probs[:,1:]
 
-f = h5py.File(out_name, "w")
-ids = f.create_dataset("ID", (len(ID),), dtype='i', compression='lzf')
-pdfs = f.create_dataset("PDF", (zarr[0],len(ID)), dtype='f', compression='lzf')
+    # z-array is assumed to be linear intervals in redshift.
+    # [n_interval, centre of lowest bin, centre of highest bin]
+    zarr = [config['N_BIN'], config['MIN_z'], config['Dz']]
 
-ids.attrs['N_objects'] = len(ID)
-pdfs.attrs['PDF_length'] = zarr[0]
-pdfs.attrs['PDF_interval'] = zarr[1]
-pdfs.attrs['PDF_start'] = zarr[2]
+    f = h5py.File(out_name, "w")
+    ids = f.create_dataset("ID", (len(ID),), dtype='i', compression='lzf')
+    pdfs = f.create_dataset("PDF", (zarr[0],len(ID)), dtype='f', compression='lzf')
 
-ids = ID
-pdfs = probs
+    ids.attrs['N_objects'] = len(ID)
+    pdfs.attrs['PDF_length'] = zarr[0]
+    pdfs.attrs['PDF_interval'] = zarr[1]
+    pdfs.attrs['PDF_start'] = zarr[2]
 
-f.close()
+    ids = ID
+    pdfs = probs
 
-# clean up by deleting the BPZ input .fits and .column files, and the BPZ outputs, .bpz and .probs
-subprocess.call(....)
+    f.close()
+
+    # clean up by deleting the BPZ input .fits and .column files, and the BPZ outputs, .bpz and .probs
+    subprocess.call("rm {0} {1} *.bpz *.probs".format(fname.split('.')+'.fits',
+                                                      fname.split('.')+'.columns'), shell=True)
